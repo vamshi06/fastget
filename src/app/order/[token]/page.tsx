1dd9da8037b1e8e20af3b58e25b83c097bbe7c1d
akankshahake-elemantra <akanksha.hake@elemantra.in>
2026-07-17 17:50:59 +0530

    call support button

--- src/app/order/[token]/page.tsx
+++ src/app/order/[token]/page.tsx
@@ -471,7 +471,7 @@ export default function OrderStatusPage() {
                 Contact our support team if you have any questions about your order.
               </p>
               <a
-                href="tel:+919999999999"
+                href="tel:+919167119131"
                 className="btn-primary w-full py-2.5 text-sm"
               >
                 <Phone className="w-4 h-4" />
